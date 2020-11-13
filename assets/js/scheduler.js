924e6fbff5e973cc97cb8e40cef3dd9791c1610c
JRob <72467031+Jonathan-84@users.noreply.github.com>
2020-11-13 14:51:17 -0500

    built out general page format- styling and functionality needed

--- assets/js/scheduler.js
+++ assets/js/scheduler.js
@@ -1,7 +1,12 @@
 // place current date in the top
 //need to fix the html part---
-let now = moment();
-let date = now.format();
+const now = moment();
+const date = now.format();
 ///console.log(now.format("dddd, MMMM Do YYYY"));
 document.getElementById("currentDay").innerHTML= now.format("dddd, MMMM Do YYYY");
 
+//functions to create: createAppt(), loadAppt() for local storage ad saveAppt(), auditAppt()
+let createAppt= function(apptText) {
+}
+
+console.log (document.querySelectorAll('.col-sm-8'));
