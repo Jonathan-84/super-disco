57e5b74d856f655b1c2331615f624dc993128d69
JRob <72467031+Jonathan-84@users.noreply.github.com>
2020-11-13 11:17:49 -0500

    added previously done basic structure

---
+++ assets/js/scheduler.js
@@ -0,0 +1,7 @@
+// place current date in the top
+//need to fix the html part---
+let now = moment();
+let date = now.format();
+console.log(now.format("dddd, MMMM Do YYYY"));
+document.getElementById("currentDay").innerHTML=date;
+
