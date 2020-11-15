03e3821335be88f996245bfbc3ea1ae47528ea4f
JRob <72467031+Jonathan-84@users.noreply.github.com>
2020-11-15 00:48:34 -0500

    added the styling as dictated by time, added the ability to store tasks locally

--- assets/js/scheduler.js
+++ assets/js/scheduler.js
@@ -7,13 +7,61 @@ document.getElementById("currentDay").innerHTML= now.format("dddd, MMMM Do YYYY"
 
 //functions to create: saveAppt(), loadAppt() for local storage, auditAppt()
 
+$(document).ready(function() {
 
   $(".saveBtn").on("click", function (){
     let value = $(this).siblings(".description").val()
     let time = $(this).parent().attr("id")
-    localStorage.setItem(value,time)
+    localStorage.setItem(value,time);
     });
   
+    function timeUpdate (){
+      let currentTime=moment().hours();
+      //loop of time
+      $(".time-block").each(function() {
+        let blockTime =parseInt($(this).attr("id").split("-")[1]);
+
+      //compare where the time is
+      if (blockTime < currentTime) {
+        $(this).addClass("past");
+      }
+      else if (blockTime === currentTime) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+      }
+      else {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+      }
+    });
+    }
+    timeUpdate();
+
+    //time updates?
+    let interval= setInterval(timeUpdate,15000);
+console.log(interval);
+    // load local storage
+    $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+    $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+    $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+    $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+    $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+    $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+    $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+    $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+    $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+    $("#hour-18 .description").val(localStorage.getItem("hour-18"));
+  });
+
+function loadStorage(){
+  let event = localStorage.getItem(value,time);
+  if (event == null || event=="null"){
+    localStorage.setItem(value,time);
+  } else {
+    localStorage.getItem(value,time);
+  }
+}
   //let appointment= JSON.parse(localStorage.getItem("appointment"))
 //if (appointment == null && appointment == 'null') {
    // events.push(currentEvent)
@@ -27,6 +75,10 @@ document.getElementById("currentDay").innerHTML= now.format("dddd, MMMM Do YYYY"
     //localStorage.setItem('appointment',JSON.stringify(appointment))
 //};
 /*
+
+
+
+
 $('description').on('click', function(event) {
 event.preventDefault();
 let currentEvent= $('description').val().trim();
