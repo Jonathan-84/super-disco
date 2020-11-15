52d084346f2067f82cc48dd467af9dc561c1b6c4
JRob <72467031+Jonathan-84@users.noreply.github.com>
2020-11-14 23:23:30 -0500

    got it to load to local storage

--- assets/js/scheduler.js
+++ assets/js/scheduler.js
@@ -5,8 +5,33 @@ const date = now.format();
 ///console.log(now.format("dddd, MMMM Do YYYY"));
 document.getElementById("currentDay").innerHTML= now.format("dddd, MMMM Do YYYY");
 
-//functions to create: createAppt(), loadAppt() for local storage ad saveAppt(), auditAppt()
-let createAppt= function(apptText) {
-}
+//functions to create: saveAppt(), loadAppt() for local storage, auditAppt()
 
-console.log (document.querySelectorAll('.col-sm-8'));
+
+  $(".saveBtn").on("click", function (){
+    let value = $(this).siblings(".description").val()
+    let time = $(this).parent().attr("id")
+    localStorage.setItem(value,time)
+    });
+  
+  //let appointment= JSON.parse(localStorage.getItem("appointment"))
+//if (appointment == null && appointment == 'null') {
+   // events.push(currentEvent)
+ //   localStorage.setItem('appointment',JSON.stringify(appointment))
+//}      
+   // let currentEvent={
+     //   description:description.value,
+       // hour: hour.value
+   // }
+  //  events.push(currentEvent)
+    //localStorage.setItem('appointment',JSON.stringify(appointment))
+//};
+/*
+$('description').on('click', function(event) {
+event.preventDefault();
+let currentEvent= $('description').val().trim();
+appointment.push(currentEvent);
+renderEvents(appointment)
+localStorage.setItem('appointment',JSON.stringify(appointment));
+$('description').val('');
+*/
